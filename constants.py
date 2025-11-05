39c4f15a6b30c9283b0bf147de0d64d0315bf88c
lucmcmullan <luc2006mcmullan@gmail.com>
2025-11-05 15:24:09 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,60 +1,59 @@
 AI_TRAFFIC = False
 
-WIDTH, HEIGHT, FPS = 1500, 600, 30
+WIDTH, HEIGHT = 1500, 600
+FPS = 30
 SIM_SPEED = 5.0
+
 RADAR_CENTER = (WIDTH // 2, HEIGHT // 2)
 SIDEBAR_WIDTH = 260
-RADAR_WIDTH = WIDTH - SIDEBAR_WIDTH
-NM_PER_PX = 0.08
-SAFE_LAT_NM, SAFE_VERT_FT = 3.0, 1000
 SIDEBAR_OFFSET = 10
+RADAR_WIDTH = WIDTH - SIDEBAR_WIDTH
 
-FIXES = {
-    "OBK":   {"x": 300,  "y": 600},
-    "DPA":   {"x": 1200, "y": 250},
-    "EON":   {"x": 800,  "y": 700},
-    "FONTI": {"x": 300,  "y": 150},
-}
-
-RUNWAYS = [
-    {"x": 0.5, "y": 0.6, "bearing": 270, "length_nm": 2.5},
-    {"x": 0.7, "y": 0.65, "bearing": 270, "length_nm": 2.5},
-
-]
+NM_PER_PX = 0.08
+SAFE_LAT_NM = 3.0
+SAFE_VERT_FT = 1000
 
 LANDING_HEADING_OFFSET_DEG = 60
 LANDING_HEIGHT_OFFSET_FT = 3000
 
 ERROR_LOG_FILE = "error_log.txt"
 
+FIXES = {
+    "DPA":   {"x": 1200, "y": 250},
+    "EON":   {"x": 800,  "y": 700},
+    "FONTI": {"x": 300,  "y": 150},
+    "OBK":   {"x": 300,  "y": 600},
+}
+
+RUNWAYS = [
+    {"x": 0.5, "y": 0.6,  "bearing": 270, "length_nm": 2.5},
+    {"x": 0.7, "y": 0.65, "bearing": 270, "length_nm": 2.5},
+]
+
 AIRLINES = {
     "British Airways": {
         "IATA": "BA",
         "ICAO": "BAW",
-        "Callsign": "Speedbird"
+        "Callsign": "Speedbird",
     },
-
     "EasyJet": {
         "IATA": "U2",
         "ICAO": "EZY",
-        "Callsign": "Easy"
+        "Callsign": "Easy",
     },
-
     "Loganair": {
         "IATA": "LM",
         "ICAO": "LOG",
-        "Callsign": "Logan"
+        "Callsign": "Logan",
     },
-
     "Ryanair": {
         "IATA": "RK",
         "ICAO": "RUK",
-        "Callsign": "Bluemax"
+        "Callsign": "Bluemax",
     },
-    
     "Wizz Air": {
         "IATA": "W9",
         "ICAO": "WUK",
-        "Callsign": "Wizz Go"
+        "Callsign": "Wizz Go",
     },
 }
